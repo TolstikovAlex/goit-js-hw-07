53af4e446246c555fa43903c7bad2f75bc6fba3c
Alex Tolstikov <tolstikovalexander2@gmail.com>
2022-07-27 18:13:35 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('ul.gallery');
+console.log(galleryEl);
+
+const listEl = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`,
+  )
+  .join('');
+
+//   Вводим галлерею на страницу
+galleryEl.insertAdjacentHTML('afterbegin', listEl);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
